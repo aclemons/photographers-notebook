2fd1cb47a43443f0335518d6f9af9984d30d2962
Alexander Boden <a.boden@freenet.de>
2014-03-16 11:34:36 +0100

    Implemented context menues for FotoContentActivity (needs testing).

--- Photographers/src/unisiegen/photographers/activity/FotoContentActivity.java
+++ Photographers/src/unisiegen/photographers/activity/FotoContentActivity.java
@@ -1,6 +1,9 @@
 package unisiegen.photographers.activity;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -10,10 +13,15 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.viewpagerindicator.TitlePageIndicator;
 import com.viewpagerindicator.TitleProvider;
@@ -26,6 +34,11 @@ import unisiegen.photographers.model.Bild;
 import unisiegen.photographers.model.Film;
 
 public class FotoContentActivity extends PhotographersNotebookActivity {
+
+    private TitlePageIndicator mIndicator;
+    private MyPagerAdapter adapter;
+    private ViewPager viewPager;
+    private Film film;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +55,7 @@ public class FotoContentActivity extends PhotographersNotebookActivity {
         int selectedItem = 0;
         selectedItem = getIntent().getExtras().getInt("selectedItem");
 
-        Film film = new Film();
+        film = new Film();
 
         if (filmID.length() > 0) {
             film = DB.getDB().getFilm(getApplicationContext(), filmID); }
@@ -52,18 +65,82 @@ public class FotoContentActivity extends PhotographersNotebookActivity {
         Drawable drawable = new BitmapDrawable(getResources(), b);
         getActionBar().setIcon(drawable);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-        MyPagerAdapter adapter = new MyPagerAdapter(getApplicationContext(), film);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        adapter = new MyPagerAdapter(getApplicationContext(), film);
         viewPager.setAdapter(adapter);
         viewPager.getAdapter().setPrimaryItem(viewPager, 2, null);
 
-        TitlePageIndicator mIndicator = (TitlePageIndicator) findViewById(R.id.indicator);
+        mIndicator = (TitlePageIndicator) findViewById(R.id.indicator);
         mIndicator.setViewPager(viewPager);
         mIndicator.setFooterColor(0xFF000000);
 
         viewPager.setCurrentItem(selectedItem);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.fotomenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.opt_openSettings) {
+            Intent openSettings = new Intent(getApplicationContext(),
+                    EditSettingsActivity.class);
+            startActivityForResult(openSettings, 0);
+            return true;
+        } else if (item.getItemId() == R.id.opt_editfoto) {
+            Intent editFoto = new Intent(getApplicationContext(), NewPictureActivity.class);
+            editFoto.putExtra("picToEdit", adapter.getTitle(viewPager.getCurrentItem()));
+            startActivity(editFoto);
+            return true;
+        } else if (item.getItemId() == R.id.opt_deletefoto) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(getString(R.string.question_delete));
+            builder.setCancelable(false);
+            builder.setPositiveButton(getString(R.string.yes),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        final String bildToDelete = adapter.getTitle(viewPager.getCurrentItem());
+                        int lastBild = 0;
+                        for (Bild bild : film.Bilder) {
+                            if (bild.Bildnummer.equals(bildToDelete)) {
+                                DB.getDB().deletePicture(
+                                        getApplicationContext(), film,
+                                        bild);
+                                break;
+                            }
+                            lastBild = film.Bilder.indexOf(bild);
+                        }
+                        finish();
+                        Intent reload = new Intent(getApplicationContext(), FotoContentActivity.class);
+                        reload.putExtra("ID", film.Titel);
+                        reload.putExtra("selectedItem", lastBild);
+                        startActivity(reload);
+                    }
+                });
+            builder.setNegativeButton(getString(R.string.no),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(
+                            DialogInterface dialog,
+                            int which) {
+
+                    }
+                });
+            AlertDialog alert = builder.create();
+            alert.show();
+
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     /*
          * Pageadapter f�r das hin- und herwischen zwischen den Bildenr. W�hlt
